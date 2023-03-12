55bf1ff6f5d32d69a780e42b87fc0ccfba561dce
Yan <mahuke777@gmail.com>
2023-03-12 13:36:07 +0300

    com

---
+++ IsPrim.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int res = 0;
+    int n;
+    cin >> n;
+    _asm {
+        mov eax, n
+        mov ebx, 2
+        for1: cdq
+        idiv ebx
+        mov eax, n
+        cmp ebx, n
+        je endll
+        add ebx, 1
+        cmp edx, 0
+        jne for1
+        endll :
+        cmp ebx, n
+            jne end1t
+            mov res, 1
+            end1t : nop
+    }
+    if (res == 1) {
+        cout << "prim";
+    }
+    else {
+        cout << "not prim";
+    }
+}
+
